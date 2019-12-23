d544d37ff2493bccef27e876e996eb4e5f3c9e44
yuchenguang0814 <546448821@qq.com>
2019-12-23 22:30:51 +0800

    shoplogin

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div id="app">
-    <router-view>hello</router-view>
+    <router-view></router-view>
+  </div>
 </template>
-
 <script>
 export default {
-  name: 'app',
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
